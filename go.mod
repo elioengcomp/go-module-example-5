361588dec5343cffa863b35d3eb1393da5e12570
Elio Marcolino <eliom@jfrog.com>
2019-09-17 09:21:54 -0700

    Added unavailable dependency

--- go.mod
+++ go.mod
@@ -1,2 +1,3 @@
 module github.com/elioengcomp/go-module-example-5
 
+require github.com/sirupsen/logrus v1.99.0
---
+++ tool/tool.go
@@ -0,0 +1,9 @@
+package tool
+
+import (
+	"strings"
+)
+
+func doWork(arg string) string {
+	return strings.ToUpper(arg)
+}
